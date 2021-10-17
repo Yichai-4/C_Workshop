8d74f3b5da5169d7095f4a3a798d59823f89c17c
Yichai-4 <hazanyichi@gmail.com>
2021-10-17 15:43:32 +0300

    Finish Exercise 1.1

--- Exercise_1/Exercise_1.1/Warmup.h
+++ Exercise_1/Exercise_1.1/Warmup.h
@@ -1,4 +1,12 @@
 #pragma once
-//#include <stdbool.h>
 
+/** @file Warmup.h
+ *  Warmup utilities
+ */
+
+/** @brief checks if the user input isn't 'Q' or 'q'
+ * 
+ * @param userInput the char entered by the user
+ * @returns int value in terms of user's input
+ */
 int ValidInput(char userInput);
