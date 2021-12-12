ebd0569a3e03339e0ef6ea9904c3d218c7708e3e
Yichai-4 <hazanyichai@gmail.com>
2021-12-12 18:41:31 +0200

    Continue implementing functions

--- Exercise_6/Exercise_6.4/LinkedList.c
+++ Exercise_6/Exercise_6.4/LinkedList.c
@@ -40,7 +40,7 @@ void FreeList(List* list)
 size_t GetListSize(List* list)
 {
 	int size = 0;  // Initialize count
-	struct Node* current = list->head;  // Initialize current
+	Node *current = list->head;  // Initialize current
 	while (current != NULL)
 	{
 		++size;
@@ -67,7 +67,24 @@ ListDataType GetValue(Node* node)
 
 Node* Insert(Node* after, ListDataType newValue)
 {
+	/*1. check if the given prev_node is NULL */
+	if (after == NULL)
+	{
+		printf("the given previous node cannot be NULL");
+		return;
+	}
 
+	/* 2. allocate new node */
+	Node *newNode = (Node*)malloc(sizeof(Node));
+
+	/* 3. put in the data */
+	newNode->value = newValue;
+
+	/* 4. Make next of new node as next of prev_node */
+	newNode->next = after->next;
+
+	/* 5. move the next of prev_node as new_node */
+	after->next = newNode;
 }
 
 Node* PushFront(List* list, ListDataType newValue)
@@ -87,14 +104,117 @@ Node* PushFront(List* list, ListDataType newValue)
 	return newNode;
 }
 
-Node* PushBack(List* list, ListDataType newValue);
+Node* PushBack(List* list, ListDataType newValue)
+{
+	//create a new node
+	Node *newNode = malloc(sizeof(Node));
+	newNode->value = newValue;
+	newNode->next = NULL;
 
-ListDataType PopFront(List* list);
+	//if head is NULL, it is an empty list
+	if (list->head == NULL)
+		list->head = newNode;
 
-ListDataType PopBack(List* list);
+	return newNode;
+}
 
-void DeleteListNode(List* list, Node* target);
+ListDataType PopFront(List* list)
+{
+	// underflow condition
+	if (list->head == NULL) {
+		return -1;
+	}
 
-Node* DeleteListValue(List* list, ListDataType value);
+	Node *head = list->head;
+	int result = head->value;        // pull out data before the node is deleted
 
-Node* FindInList(List* list, ListDataType value);
+	(list->head) = (list->head)->next;  
+
+	free(head);             // free the head node
+
+	return result;
+}
+
+ListDataType PopBack(List* list)
+{
+	if (list->head == NULL)
+		return NULL;
+
+	if (list->head->next == NULL) {
+		return list->head;
+	}
+
+	// Find the second last node
+	Node* second_last = list->head;
+	while (second_last->next->next != NULL)
+		second_last = second_last->next;
+
+	// Delete last node
+	delete (second_last->next);
+
+	// Change next of second last
+	second_last->next = NULL;
+
+	return list->head;
+}
+
+void DeleteListNode(List* list, Node* target)
+{
+
+	// If linked list is empty
+	if (list->head == NULL)
+		return;
+
+	// Store head node
+	Node *temp = list->head;
+
+	// If head needs to be removed
+	if (target == 0)
+	{
+		list->head = temp->next;   // Change head
+		free(temp);               // free old head
+		return;
+	}
+
+	// Find previous node of the node to be deleted
+	for (int i = 0; temp != NULL && i < target - 1; i++)
+		temp = temp->next;
+
+	// If position is more than number of nodes
+	if (temp == NULL || temp->next == NULL)
+		return;
+
+	// Node temp->next is the node to be deleted
+	// Store pointer to the next of node to be deleted
+	Node *next = temp->next->next;
+
+	// Unlink the node from linked list
+	free(temp->next);  // Free memory
+
+	temp->next = next;
+}
+
+Node* DeleteListValue(List* list, ListDataType value)
+{
+	for (size_t i = 0; i < GetListSize; ++i)
+	{
+		if (list->current->value == value)
+		{
+			DeleteListNode(list, list->current);
+		}
+	}
+
+	return list->current;
+}
+
+Node* FindInList(List* list, ListDataType value)
+{
+	Node *current = list->head;  // Initialize current
+	while (current != NULL)
+	{
+		if (current->value == value)
+			return current;
+		current = current->next;
+	}
+	return NULL;
+}
